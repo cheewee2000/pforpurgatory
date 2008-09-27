facc3f39de5b401003832488fea13eff02d0d60d
cwwang <cwwang@e1988b74-8cba-11dd-aa02-45bbecb07bf0>
2008-09-27 17:41:43 +0000

    initial import

---
+++ pForPurgatory04/src/bots.cpp
@@ -0,0 +1,308 @@
+/*
+ *  bots.cpp
+ *  openFrameworks
+ *
+ *  Created by cw wang on 6/4/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "bots.h"
+
+void Bots::setup()
+{
+	
+	x=ofRandom(0,ofGetWidth());
+	y=ofRandom(0,ofGetHeight());
+	px=x;
+	py=y;
+	vx=0;
+	vy=0;
+	//size=3;
+	maxSize=6.0;
+	size=ofRandom(1, 3);
+	maxDepth=size*2.0;
+	g=1;
+	//gmin=1;
+	//gmax=11;
+	
+	float d=ofRandom(0,1);
+	if(d<.5)dir=-1;
+	else dir=1;
+	
+	//velocity=4;
+	velocity=5.0/size*dir;
+	//velocity=0;
+	
+	vmin=0;
+	vmax=6;
+	falling=true;
+	downhill=false;
+	normalAngle=PI;
+	fallCount=0;
+	alive=true;
+	aliveCount=0;
+	lifeLength=ofRandom(500,3000);
+	
+	deathCount=0;
+	graveBound=false;
+	nFeetUnder=0;
+	rotCount=0;
+	rotting=false;
+	ashes=false;
+	growth=0;
+	isTree=false;
+	eating=false;
+	isBeingEatenBy=-1;
+}
+
+void Bots::update()
+{
+	
+	if (y>=py)
+	{
+		g*=1.04;
+		downhill=true;
+	}
+	
+	if (y<py) 
+	{
+		g=1.0;
+		downhill=false;
+	}
+	
+	//normal running around
+	if(alive && eating==false)
+	{
+		x+=vx;
+		y+=g;	
+		
+		//min max velocity
+		if(fabs(velocity)<vmin) velocity=vmin*dir; 
+		if(fabs(velocity)>vmax) velocity=vmax*dir;
+	
+		//find normal 
+		float angle=atan2( (py-y),(px-x));
+		if(velocity>0)	normalAngle=angle+PI/2.0;
+		else normalAngle=angle-PI/2.0;
+		
+		vx=velocity*fabs(cos(angle));
+		
+		aliveCount++;
+		if(aliveCount % 300 == 0)size+=.3;
+		if(aliveCount>=lifeLength) die();
+	}
+	
+	//fell to death
+	else if (alive==false && deathCount<50)
+	{
+		velocity=0;
+		y+=g;	
+		deathCount++;
+	}
+	//rotting
+	else if( ashes==false)
+	{
+		maxDepth=size*2.0;
+		
+		velocity=0;
+		nFeetUnder+=.03;
+		if (nFeetUnder>=maxDepth)
+		{
+			nFeetUnder=maxDepth;
+			rotCount++;
+			rotting=true;
+		}
+		graveBound=true;
+	}
+	//grow food
+	else if (ashes && beingEaten==false && isTree==false)
+	{
+		
+		growth+=.1;
+		size-=.05;
+		nFeetUnder-=.03;
+		
+		if(growth>maxDepth*2.0)
+		{
+			growth=maxDepth*2.0;
+			isTree=true;
+		}
+	}
+	//eating
+	if(eating)
+	{
+		velocity=0;
+		size+=.005;
+		nFeetUnder=0;
+	}
+	if(isBeingEatenBy!=-1)
+	{
+		growth-=.1;
+		//nFeetUnder-=.03;
+		
+	}
+
+	//alive bots find partners
+	//bots give birth
+	//bots build stuff
+	
+	px=x;
+	py=y;
+	
+	if (size>maxSize) size=maxSize;
+	else 	if (size<.1) size=0.1;
+	
+	if(x>ofGetWidth()+5) x=0;
+	if(x<-5) x=ofGetWidth();
+	
+}
+
+void Bots::fallingToDeath()
+{
+	
+}
+
+void Bots::die()
+{
+	alive=false;
+	
+}
+
+void Bots::fallingSetTrue()
+{
+	falling=true;	
+	ofFill();
+	//ofSetColor(210,210,210);
+	pSetHSV(80,.5,.85);
+	
+	fallCount++;
+	
+	//printf("%d",fallCount);
+}
+
+void Bots::fallingSetFalse()
+{	
+	
+	falling=false;	
+	ofFill();
+	pSetHSV(80,.5,.7);
+	
+	if(fallCount>100)
+	{
+		die();
+	}
+	else if (alive)
+	{
+		fallCount=0;
+	}
+	
+}
+
+void Bots::shiftPos(float X, float Y)
+{
+	x+=X;
+	y-=Y;
+	//printf("%f,%f\n",x,y);
+}
+
+void Bots::draw()
+{
+	if(alive)
+	{
+		//ofEllipse(x+cos(normalAngle)*size,y+sin(normalAngle)*size, size,size);
+		float headX=x+cos(normalAngle)*size*4.0;
+		float headY=y+sin(normalAngle)*size*4.0;
+		ofEllipse(headX,headY, size,size);
+		//normal line
+		ofLine(x, y, headX, headY);
+	}
+	else if (alive==false && ashes==false)
+	{
+		float value=.3+(float)5.0/(int)rotCount;
+		if(value>.6)value=.6;
+		if(value<.33) ashes=true;
+		pSetHSV(30,.9,value);
+		ofEllipse(x,y+size, size,size);
+	}
+	else if (ashes)
+	{
+		drawTree();		
+		pSetHSV(30,.9,.33); //dirt brown
+		ofEllipse(x,y+size, size,size);
+		
+	}
+}
+
+void Bots::drawTree()
+{
+	
+	//pSetHSV(30,.9,.25); //dirt brown
+	//ofLine(x,y,x,y-growth);
+	
+	ofFill();
+	//ofNoFill();
+	pSetHSV(120,.9,.6); //green
+	
+	//for (float i=0; i<growth; i=i+growth/10)
+	//	{
+	/*
+	 float x1= x+cos(normalAngle)*maxDepth*2.0;
+	 float y1= y+sin(normalAngle)*maxDepth*2.0;
+	 float x2= x+cos(normalAngle)*maxDepth*2.0;
+	 float y2= y-growth*9.0;
+	 float x3= x;
+	 float y3= y-growth*10.0;
+	 */
+	
+	float w=.5;
+	int d=-1;
+	
+	for ( int i=0; i<2; i++)
+	{
+		if(i==0)
+		{
+			d=1;
+			w=.3;
+		}
+		if(i==1)
+		{
+			w=.5;
+			d=-1;
+		}
+		float x1= x;
+		float y1= y-growth*.33;
+		float x2= x-growth*w*.33*d;
+		float y2= y-growth*.66;
+		float x3= x+growth*w*d;
+		float y3= y-growth;
+		
+		ofBezier(x,y,x1,y1,x2,y2,x3,y3);	
+	}
+	
+	//	}
+	
+	/*
+	 //if(isTree)
+	 //{
+	 //ofEllipse(x,y-growth,size,size);
+	 float scale=3;
+	 float maxScale=scale;
+	 for( int i=1;i<6; i++)
+	 {
+	 scale-=.7;
+	 float ypos=y+scale/maxScale;
+	 
+	 float x1= x-growth/maxScale*scale;
+	 float x2= x;
+	 float x3= x+growth/maxScale*scale;
+	 float y1=ypos-growth/2.0*i;
+	 float y2=ypos-growth/2.0*i-(growth/maxScale/2.0)*scale;
+	 float y3=ypos-growth/2.0*i;
+	 
+	 ofTriangle(x1,y1,x2,y2,x3,y3);
+	 }
+	 //}
+	 */	
+}
+
---
+++ pForPurgatory04/src/testApp.cpp
@@ -0,0 +1,386 @@
+#include "testApp.h"
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+	
+#ifdef _USE_LIVE_VIDEO
+	vidGrabber.setVerbose(true);
+	vidGrabber.initGrabber(320,240);
+#else
+	vidPlayer.loadMovie("fingers.mov");
+	vidPlayer.play();
+#endif
+	
+    colorImg.allocate(320,240);
+	grayImage.allocate(320,240);
+	grayBg.allocate(320,240);
+	grayDiff.allocate(320,240);
+	
+	bLearnBakground = true;
+	threshold = 80;
+	
+	//set intial anchor y positions
+	for ( int h=0; h<nAnchors; h++) //for each anchor point
+	{
+		curveAnchorY[h]=ofGetHeight();		
+	}
+	
+	//set initial bot positions
+	for ( int i=0; i<nBots; i++) //for each anchor point
+	{
+		bots[i].setup();		
+	}
+	
+	helveticaSM.loadFont("Helvetica Neue.ttf", 7);
+	
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	//ofBackground(100,100,100);
+	
+    bool bNewFrame = false;
+	
+#ifdef _USE_LIVE_VIDEO
+	vidGrabber.grabFrame();
+	bNewFrame = vidGrabber.isFrameNew();
+#else
+	vidPlayer.idleMovie();
+	bNewFrame = vidPlayer.isFrameNew();
+#endif
+	
+	if (bNewFrame){
+		
+#ifdef _USE_LIVE_VIDEO
+		colorImg.setFromPixels(vidGrabber.getPixels(), 320,240);
+#else
+		colorImg.setFromPixels(vidPlayer.getPixels(), 320,240);
+#endif
+		
+        grayImage = colorImg;
+		if (bLearnBakground == true){
+			grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
+			bLearnBakground = false;
+		}
+		
+		// take the abs value of the difference between background and incoming and then threshold:
+		grayDiff.absDiff(grayBg, grayImage);
+		grayDiff.threshold(threshold);
+		
+		// find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+		// also, find holes is set to true so we will get interior contours as well....
+		contourFinder.findContours(grayDiff, 20, (340*240)/3, 10, true);	// find holes
+	}
+	
+	//printf("%f \n", ofGetFrameRate());
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	
+	pBackground();
+	
+	// draw the incoming, the grayscale, the bg and the thresholded difference
+	//ofSetColor(255,255,255);
+	//colorImg.draw(20,20);	
+	//grayImage.draw(360,20);
+	//grayBg.draw(20,280);
+	
+	//grayDiff.draw(0,ofGetHeight()-240);
+	float contourScale = ofGetWidth()/320.0;	
+	
+	/*
+	 
+	 //contour finder
+	 // ---------------------------- draw the bounding rectangle
+	 ofSetColor(105,105,105);
+	 glPushMatrix();
+	 //glTranslatef( 0, 0, 0.0 );
+	 
+	 ofNoFill();
+	 for( int i=0; i<contourFinder.blobs.size(); i++ ) {
+	 ofRect( ofGetWidth() - contourFinder.blobs[i].boundingRect.x*contourScale, contourFinder.blobs[i].boundingRect.y*contourScale,
+	 - contourFinder.blobs[i].boundingRect.width*contourScale, contourFinder.blobs[i].boundingRect.height*contourScale );
+	 }
+	 */
+	
+	/*
+	 // ---------------------------- draw the blobs
+	 //ofSetColor(255,20,0);
+	 ofSetColor(98,98,98);
+	 ofFill();
+	 for( int i=0; i<contourFinder.blobs.size(); i++ ) {
+	 ofBeginShape();
+	 for( int j=0; j<contourFinder.blobs[i].nPts; j++ ) {
+	 ofVertex(ofGetWidth() - contourFinder.blobs[i].pts[j].x*contourScale, contourFinder.blobs[i].pts[j].y*contourScale );
+	 }
+	 ofEndShape();
+	 }
+	 glPopMatrix();
+	 */
+	
+	//find peaks and valleys
+	
+	for ( int h=0; h<nAnchors; h++) //for each anchor point
+	{
+		curveAnchorX[h]=ofGetWidth()/(float)(nAnchors-1)*h;
+		
+		for( int i=0; i<contourFinder.blobs.size(); i++ ) //check each blob 
+		{
+			for( int j=0; j<contourFinder.blobs[i].nPts; j++ ) //check all points in blob
+			{
+				float contX=ofGetWidth() - contourFinder.blobs[i].pts[j].x*contourScale; 
+				if (h>=0)
+				{
+					if (contX>=curveAnchorX[h]-ofGetWidth()/nAnchors/2.0 && contX<=curveAnchorX[h]+ofGetWidth()/nAnchors/2.0) //if blob x position is within anchor point
+					{
+						float contY=contourFinder.blobs[i].pts[j].y*contourScale;
+						if(contY < curveAnchorY[h]) //find the highest point
+						{
+							curveAnchorY[h]=smooth(contY,.99, curveAnchorY[h]);  //smooth changes from previous reading
+						}
+						
+					}
+				}
+			}
+		}
+	}
+	
+	//draw anchors
+	float horizon=ofGetHeight()-50;
+	
+	//ofSetColor(0,0,0);
+	for ( int h=0; h<nAnchors; h++)
+	{
+		//ofEllipse(curveAnchorX[h], curveAnchorY[h], 4,4);
+		curveAnchorY[h]=smooth(ofGetHeight(),.95, curveAnchorY[h]);
+		if(curveAnchorY[h]>horizon) curveAnchorY[h]=horizon;
+		
+	}
+	
+	//draw hills and valleys
+	ofFill();
+	pSetHSV(30,.9,.3);
+	
+	ofBeginShape();
+	ofCurveVertex(0,horizon);
+	ofCurveVertex(0,ofGetHeight());
+	for ( int i=0; i<nAnchors; i++)
+	{
+		//draw lines through points
+		ofCurveVertex(curveAnchorX[i],curveAnchorY[i]);
+		
+	}
+	ofCurveVertex(ofGetWidth(),ofGetHeight());
+	ofCurveVertex(ofGetWidth(),horizon);
+	ofEndShape();
+	
+	//update bot positions
+	
+	for ( int i=0; i<nBots; i++) //for each bot point
+	{
+		bots[i].update();
+		
+		//check bots y position to contour
+		bots[i]=pContourCheck(0,ofGetHeight(), bots[i]);
+		for ( int j=0; j<nAnchors; j++) bots[i]=pContourCheck(curveAnchorX[j],curveAnchorY[j], bots[i]);
+		bots[i]=pContourCheck(ofGetWidth(),ofGetHeight(), bots[i]);
+		
+		
+		//alive bots eat food
+		if(bots[i].alive && bots[i].falling==false)
+		{
+			for (int j=0; j<nBots; j++)
+			{
+				
+				if(bots[i].x<bots[j].x+3 && bots[i].x>bots[j].x-3)
+				{
+					if(bots[j].isTree) 
+					{
+						if (bots[j].isBeingEatenBy==i || bots[j].isBeingEatenBy==-1)
+						{
+							bots[i].eating=true;
+							
+							bots[i].x=bots[j].x;
+							bots[j].isBeingEatenBy=i;
+						
+							if(bots[j].growth<0)
+							{
+								bots[i].eating=false;	 
+								bots[i].velocity=bots[i].maxSize/bots[i].size*bots[i].dir;
+								bots[j].growth=0;
+								bots[j].setup();
+							}
+					
+						 }
+					}
+					else
+					{
+						bots[i].velocity=bots[i].maxSize/bots[i].size*bots[i].dir;						
+					}
+				}
+				
+				
+				
+				
+			}
+		}
+		
+		
+		bots[i].draw();		
+		
+		//draw info here because you only want to load the font once
+		/*
+		if(bots[i].alive)//==false)
+		{
+			ofSetColor(150,150,150);
+			char reportStr[1023];
+			//sprintf(reportStr, "fallCount: %i \n", bots[i].fallCount);
+			//sprintf(reportStr, "nFeetUnder: %f \ngrowth: %f \nisBeingEatenBy: %d \n", bots[i].nFeetUnder, bots[i].growth, bots[i].isBeingEatenBy);
+			sprintf(reportStr, "aliveCount: %d \nsize: %f", bots[i].aliveCount, bots[i].size);
+			helveticaSM.drawString(reportStr, bots[i].x , bots[i].y);
+		}
+		 */
+	}
+	
+	
+	
+	/*	
+	 // onscreen text
+	 ofSetColor(150,150,150);
+	 char reportStr[1023];
+	 sprintf(reportStr, "press ' ' to capture bg\nthreshold %i (press: +/-)\nnum blobs found %i", threshold, contourFinder.nBlobs);
+	 ofDrawBitmapString(reportStr, ofGetWidth()-300, ofGetHeight()-100);
+	 */
+}
+
+
+//---------------------------- and for curve vertexes, since we need 4 to make a curve
+std::vector <double*> pCurveVertices;
+
+Bots  testApp::pContourCheck(float x, float y, Bots b){
+	double* point = new double[3];
+ 	point[0] = x;
+	point[1] = y;
+	point[2] = 0;
+ 	pCurveVertices.push_back(point);
+	
+ 	if (pCurveVertices.size() >= 4){
+		
+ 		int startPos = (int)pCurveVertices.size() - 4;
+		
+ 		float x0 = pCurveVertices[startPos + 0][0];
+	   	float y0 = pCurveVertices[startPos + 0][1];
+ 		float x1 = pCurveVertices[startPos + 1][0];
+	   	float y1 = pCurveVertices[startPos + 1][1];
+ 		float x2 = pCurveVertices[startPos + 2][0];
+	   	float y2 = pCurveVertices[startPos + 2][1];
+ 		float x3 = pCurveVertices[startPos + 3][0];
+	   	float y3 = pCurveVertices[startPos + 3][1];
+		
+ 		int resolution = 50;
+		
+		float t,t2,t3;
+		float x,y;
+		
+		for (int i = 0; i < resolution; i++){
+			
+			t 	=  (float)i / (float)(resolution-1);
+			t2 	= t * t;
+			t3 	= t2 * t;
+			
+			x = 0.5f * ( ( 2.0f * x1 ) +
+						( -x0 + x2 ) * t +
+						( 2.0f * x0 - 5.0f * x1 + 4 * x2 - x3 ) * t2 +
+						( -x0 + 3.0f * x1 - 3.0f * x2 + x3 ) * t3 );
+			
+			y = 0.5f * ( ( 2.0f * y1 ) +
+						( -y0 + y2 ) * t +
+						( 2.0f * y0 - 5.0f * y1 + 4 * y2 - y3 ) * t2 +
+						( -y0 + 3.0f * y1 - 3.0f * y2 + y3 ) * t3 );
+			
+			if (b.x<=x+1 && b.x>=x-1 )
+			{
+				//float bottomX=b.x+cos(b.normalAngle+PI)*b.size;
+				//float bottomY=b.y+sin(b.normalAngle+PI)*b.size;
+				if (b.y>=y-5+b.nFeetUnder || b.graveBound)
+				{
+					float dx=b.x-x;
+					float dy=b.y-y-b.nFeetUnder;
+					
+					b.shiftPos(dx,dy);
+					b.fallingSetFalse();
+				}
+				
+				else //if(b.y<y-5)
+				{
+					b.fallingSetTrue();
+				}
+				
+				
+			}
+		}
+ 	}
+	return  b;
+}
+
+
+void testApp::pBackground()
+{
+	for (int j=0; j<ofGetHeight(); j++)
+	{			
+		pSetHSV(200,1,(float)(j)/(float)ofGetHeight());
+		//pSetHSV(360.0*(float)(j)/(float)ofGetHeight(),1,1);
+		ofLine(0,j,ofGetWidth(),j);
+	}		
+}
+
+float testApp::smooth(float raw, float smoothness, float smoothedVal){
+	if (smoothness >= 1){      // check to make sure param's are within range
+		smoothness = 1;
+	}
+	else if (smoothness <= 0){
+		smoothness = 0;
+	}
+	smoothedVal = (raw * (1 - smoothness)) + (smoothedVal  *  smoothness);
+	return smoothedVal;
+}
+
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){ 
+	
+	switch (key){
+		case ' ':
+			bLearnBakground = true;
+			break;
+		case '+':
+			threshold ++;
+			if (threshold > 255) threshold = 255;
+			break;
+			case '-':
+			threshold --;
+			if (threshold < 0) threshold = 0;
+			break;
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+}	
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(){
+	
+}
